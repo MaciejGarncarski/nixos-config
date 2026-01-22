b86e5149b8ef234b8b891f8daa1d6dcd66b3b9c9
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-01-22 12:54:05 +0100

    feat: remove p10k from config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -108,9 +108,6 @@
           autosuggestion.enable = true;
 
           initContent = ''
-            # Change based on repository location
-            source ~/.nix-config/.p10k.zsh
-
             eval "$(${pkgs.mise}/bin/mise activate zsh)"
 
             bindkey '^[[1;5D' backward-word
