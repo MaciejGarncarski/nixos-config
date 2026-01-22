5f82eca2b1ed8bfc74c37c6f8bed69eeefda4206
Maciej Garncarski <maciejgarncarski@protonmail.com>
2026-01-22 13:15:05 +0100

    refactor: plugin

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -118,6 +118,14 @@
                   '')
           ];
 
+          plugins = [
+            {
+              name = "powerlevel10k";
+              src = pkgs.zsh-powerlevel10k;
+              file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+            }
+          ];
+
           shellAliases = {
             ls = "eza";
             cat = "bat";
